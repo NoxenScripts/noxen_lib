7b84242d517cca90a4d590043e3a3cbb764dc4c0
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-08-20 19:30:25 +0200

    refactor(imports): enhance code readability and maintainability with const declarations

--- imports.lua
+++ imports.lua
@@ -1,4 +1,5 @@
 nox = {};
+_lib = nox; -- For compatibility with old code
 nox.cache = {};
 nox.classes = {};
 nox.is_server = IsDuplicityVersion();
@@ -7,20 +8,21 @@ nox.current_resource = GetCurrentResourceName();
 nox.debug = GetConvar('noxen_lib_debug', "false") == "true";
 
 if (nox.current_resource ~= nox.name) then
-
-    local state = GetResourceState(nox.name);
+    local state <const> = GetResourceState(nox.name);
 
     if (not state:find('start')) then
         print('^7(^6lib^7)^0 => ^1Unable to import noxen_lib! Please start noxen_lib before using it.^0');
+        if (nox.is_server) then
+            StopResource(nox.current_resource);
+        end
     end
-
 end
 
 local current_resource = nox.name;
 
-local modules = {};
-local _require = require;
-local _path = './?.lua;';
+local modules <const> = {};
+local _require <const> = require;
+local _path <const> = './?.lua;';
 
 ---@return string
 function nox.get_required_resource()
@@ -35,38 +37,34 @@ end
 ---@param modname string
 ---@return any
 function require(modname)
-
     if type(modname) ~= 'string' then return; end
 
-    local mod_id = ('%s.%s'):format(current_resource, modname);
+    local mod_id <const> = ('%s.%s'):format(current_resource, modname);
     local module = modules[mod_id];
 
     if (not module) then
-
         if (module == false) then
             error(("^1circular-dependency occurred when loading module '%s'^0"):format(modname), 0);
         end
 
-        local success, result = pcall(_require, modname);
+        local success <const>, result <const> = pcall(_require, modname);
 
         if (success) then
             modules[mod_id] = result;
             return result;
         end
 
-        local modpath = modname:gsub('%.', '/');
+        local modpath <const> = modname:gsub('%.', '/');
 
         for path in _path:gmatch('[^;]+') do
-
             local script = path:gsub('?', modpath):gsub('%.+%/+', '');
-            local resourceFile = LoadResourceFile(current_resource, script);
+            local resourceFile <const> = LoadResourceFile(current_resource, script);
 
             if (type(resourceFile) == 'string') then
-
                 modules[mod_id] = false;
                 script = ('@@%s/%s'):format(current_resource, script)
 
-                local chunk, err = load(resourceFile, script)
+                local chunk <const>, err <const> = load(resourceFile, script)
 
                 if (err or not chunk) then
                     modules[mod_id] = nil;
@@ -85,17 +83,11 @@ function require(modname)
                 modules[mod_id] = module;
 
                 return module;
-
             end
-
         end
-
         return error(("module ^7\'^3%s^7\'^1 not found^0"):format(modname), 0);
-
     end
-
     return module;
-
 end
 
 nox.enums = require 'enums.index';
