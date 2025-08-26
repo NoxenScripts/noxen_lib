f4fcf64d12de58ad975f03714fa723ed318e9119
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-08-26 19:13:17 +0200

    feat(bridge): enhance player data synchronization with inventory and account updates

--- lib/bridge/listeners/esx/client.lua
+++ lib/bridge/listeners/esx/client.lua
@@ -1,5 +1,49 @@
 local bridge <const> = require 'lib.bridge.bridge';
 
-bridge:RegisterNetEvent('esx', 'esx:playerLoaded', function(playerData)
-    console.log(("ESX player loaded: %s"):format(json.encode(playerData, {indent=true})));
+-- bridge:RegisterNetEvent('esx', 'esx:playerLoaded', function(playerData)
+--     console.log(("ESX player loaded: %s"):format(json.encode(playerData, {indent=true})));
+-- end);
+
+bridge:AddEventHandler('esx', 'esx:setPlayerData', function(key, value)
+    local invoking <const> = GetInvokingResource();
+
+    if (invoking ~= 'es_extended') then
+        return console.warn(("esx:setPlayerData - Invalid invoking resource ^3%s^7."):format(tostring(invoking)));
+    end
+
+    if (key ~= 'inventory') then
+        return;
+    end
+
+    bridge.player.data[key] = {};
+
+    for i = 1, #value do
+        bridge.player.data[key][#bridge.player.data[key] + 1] = value[i] and {
+            name = value[i].name,
+            label = value[i].label,
+            weight = value[i].weight,
+            amount = type(value[i].count) == 'number' and value[i].count or value[i].amount,
+            usable = type(value[i].usable) == 'boolean' and value[i].usable == true
+        } or nil;
+    end
+    console.debug(('Updated player data ^3%s^7 to ^2%s^7'):format(key, json.encode(bridge.player.data[key], {indent=true})));
+end);
+
+nox.events.on.secure('ox_inventory:updateSlots', function(_, data)
+    bridge.player.data['inventory'] = {};
+
+    for i = 1, #data do
+        if (data[i].inventory ~= nox.bridge.player.source) then
+            goto continue;
+        end
+        bridge.player.data['inventory'][#bridge.player.data['inventory'] + 1] = data[i].item and data[i].item.name and {
+            name = data[i].item.name,
+            label = data[i].item.label,
+            weight = data[i].item.weight,
+            amount = type(data[i].item.count) == 'number' and data[i].item.count or items[i].amount,
+            usable = type(data[i].item.usable) == 'boolean' and data[i].item.usable == true
+        } or nil;
+        ::continue::
+    end
+    console.debug(('(^3ox_inventory^7) Updated player data ^3%s^7 to ^2%s^7'):format('inventory', json.encode(bridge.player.data['inventory'], {indent=true})));
 end);
--- lib/bridge/listeners/esx/server.lua
+++ lib/bridge/listeners/esx/server.lua
@@ -10,6 +10,16 @@ bridge:AddEventHandler('esx', 'esx:playerLoaded', function(source, xPlayer)
 
     bridge.playersIdentifier[xPlayer.license] = player.source;
 
+    player:TriggerEvent(('noxen_lib_%s:bridge:player:setPlayerData'):format(nox.current_resource), {
+        identifier = player:GetIdentifier(),
+        job = player:GetJob(),
+        job2 = player:GetJob2(),
+        money = player:GetAccountMoney('money'),
+        black_money = player:GetAccountMoney('black_money'),
+        bank = player:GetAccountMoney('bank'),
+        inventory = player:GetInventory(),
+    });
+
     console.debug("ESX player loaded", xPlayer);
 end);
 
@@ -22,3 +32,61 @@ bridge:AddEventHandler('esx', 'esx:playerDropped', function(source)
 
     bridge.playersIdentifier[player.handle.license] = nil;
 end);
+
+---@param source number
+local function updateInventory(source)
+    local player <const> = bridge:GetPlayer(source);
+
+    if (not player) then
+        return;
+    end
+
+    player:TriggerEvent(('noxen_lib_%s:bridge:player:setPlayerData'):format(nox.current_resource), {
+        inventory = player:GetInventory()
+    });
+end
+
+bridge:AddEventHandler('esx', 'esx:onAddInventoryItem', updateInventory);
+bridge:AddEventHandler('esx', 'esx:onRemoveInventoryItem', updateInventory);
+
+---@param source number
+---@param accountName string
+local function updateAccount(source, accountName)
+    local player <const> = bridge:GetPlayer(source);
+
+    if (not player) then
+        return;
+    end
+
+    for name, account --[[ @type noxen.lib.bridge.wrapper.account ]] in pairs(bridge.wrapper.accounts) do
+        if (account.name == accountName) then
+            player:TriggerEvent(('noxen_lib_%s:bridge:player:setPlayerData'):format(nox.current_resource), {
+                [account.name] = player:GetAccountMoney(account.name)
+            });
+            return;
+        end
+    end
+end
+
+bridge:AddEventHandler('esx', 'esx:setAccountMoney', updateAccount);
+bridge:AddEventHandler('esx', 'esx:addAccountMoney', updateAccount);
+bridge:AddEventHandler('esx', 'esx:removeAccountMoney', updateAccount);
+
+---@param type 'job' | 'job2'
+local function updateJob(type)
+    ---@param source number
+    return function(source)
+        local player <const> = bridge:GetPlayer(source);
+
+        if (not player) then
+            return;
+        end
+
+        player:TriggerEvent(('noxen_lib_%s:bridge:player:setPlayerData'):format(nox.current_resource), {
+            [type] = player:getJobInternal(type)
+        });
+    end
+end
+
+bridge:AddEventHandler('esx', 'esx:setJob', updateJob('job'));
+bridge:AddEventHandler('esx', 'esx:setJob2', updateJob('job2'));
